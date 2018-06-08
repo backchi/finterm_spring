0058803007679e93e237572dd0b278835d1d09b1
backchi <ekgml106@gmail.com>
2018-06-08 19:28:52 +0900

    insert

---
+++ src/main/java/com/example/finterm/board/service/BoardService.java
@@ -0,0 +1,34 @@
+package com.example.finterm.board.service;
+
+import com.example.finterm.board.domain.BoardVO;
+import com.example.finterm.board.mapper.BoardMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service("com.example.finterm.board.service.BoardService")
+public class BoardService {
+    @Resource(name="com.example.finterm.board.mapper.BoardMapper")
+    BoardMapper mBoardMapper;
+
+    public List<BoardVO> boardListService() throws Exception{
+        return mBoardMapper.boardList();
+    }
+
+    public BoardVO boardDetailService(int bno) throws Exception{
+        return mBoardMapper.boardDetail(bno);
+    }
+
+    public int boardInsertService(BoardVO board) throws Exception{
+        return mBoardMapper.boardInsert(board);
+    }
+
+    public int boardUpdateService(BoardVO board) throws Exception{
+        return mBoardMapper.boardUpdate(board);
+    }
+
+    public int boardDeleteService(int bno) throws Exception{
+        return mBoardMapper.boardDelete(bno);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/insert.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Insert title here</title>
+</head>
+<body>
+<div class="container">
+    <form action="/insertProc" method="post">
+        <div class="form-group">
+            <label for="title">제목</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="제목을 입력하세요.">
+        </div>
+        <div class="form-group">
+            <label for="content">내용</label>
+            <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">작성</button>
+    </form>
+</div>
+</body>
+</html>
